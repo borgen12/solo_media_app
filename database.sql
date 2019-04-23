735d372f37edcb9619719c526af5cc215994d038
Chris Borgen <borgen_12@hotmail.com>
2019-04-22 23:22:43 -0500

    added images, sample video and created database tables for users and movies

--- database.sql
+++ database.sql
@@ -8,3 +8,20 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "movies" (
+	"id" SERIAL PRIMARY KEY,
+	"title" VARCHAR (80) NOT NULL,
+	"year" INTEGER,
+	"description" VARCHAR (400) NOT NULL,
+	"length" INTEGER,
+	"viewed" BOOLEAN DEFAULT FALSE,
+	"video_url" VARCHAR,
+	"image_url" VARCHAR
+); 
+
+INSERT INTO movies (title, description, "year", length, video_url, image_url)
+VALUES ('Godzilla vs Megalon', 'The undersea nation of Seatopia sends the gigantic Megalon to destroy the world above - and its up to Godzilla and a size-shifting robot, Jet Jaguar, to defeat him.', '1964', '78', 'videos/Godzilla_vs_Megalon.mp4', 'images/godzillavsmegalon.jpg'),
+('Ghidora, the Three-Headed Monster','After a meteorite unleashes a three-headed beast upon Tokyo, Mothra tries to unite with Godzilla and Rodan to battle the extraterrestrial threat.', '1964', '89', 'videos/ghidora.mp4', 'images/ghidora3.jpg')
+;
+
