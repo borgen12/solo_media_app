64395e0e9e4b05e32ac710af67fbe198414e0960
Chris Borgen <borgen_12@hotmail.com>
2019-04-23 16:02:53 -0500

    fixed function class on movieCard

--- src/components/MoviePage/MovieCard.js
+++ src/components/MoviePage/MovieCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
@@ -7,6 +7,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom'
 
 const styles = {
   card: {
@@ -15,34 +16,41 @@ const styles = {
   media: {
     height: 140,
   },
-};
+}; 
 
-/* handleClick = (event) => {
-  return event
-} */
+class MovieCard extends Component {
 
-const MovieCard = (props) => {
-  const { classes } = props;
-  return (
-    <Card className={classes.card} >
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image={props.movie.image_url}
-          title={props.movie.title}
-          
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {props.movie.title}
-          </Typography>
-          <Typography component="p">
-            {props.movie.description}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-  );
+  handleClick = (event) => {
+    console.log('clicked card', this.props.movie.id);
+    this.props.dispatch({type:'SHOW_DETAILS', paylod: this.props.movie.id})
+    this.props.history.push('/info')
+
+  }
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <Card className={classes.card} onClick={this.handleClick}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image={this.props.movie.image_url}
+            title={this.props.movie.title}
+
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {this.props.movie.title}
+            </Typography>
+            <Typography component="p">
+              {this.props.movie.description}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    );
+  }
+  
 }
 
 MovieCard.propTypes = {
